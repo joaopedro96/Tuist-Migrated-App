cfde792c8ef782c563b82ce52733ddec5455fce8
João Pedro da Mata Gonçalves Ribeiro <joaopedro.damata96@gmail.com>
2023-07-10 23:49:59 -0300

    Added settings configuration to manifest

--- Project.swift
+++ Project.swift
@@ -1,20 +1,41 @@
 
 import ProjectDescription
 
-let project = Project(
-    name: "MovieInfo",
-    organizationName: "joaoribeiroteam",
-    settings: nil,
-    targets: [
-        Target(
-            name: "MovieInfo",
-            platform: .iOS,
-            product: .app,
-            bundleId: "co.joaoribeiro",
-            infoPlist: "MovieInfo/Info.plist",
-            sources: ["MovieInfo/Source/**"],
-            resources: ["MovieInfo/Resources/**"],
-            dependencies: [],
-            settings: nil)
-    ]
-)
+// MARK: - SETTINGS
+
+let targetConfigurations: [Configuration] = [
+    .debug(name: .debug, xcconfig: Path("config/MovieInfoTarget.xcconfig")),
+    .release(name: .release, xcconfig: Path("config/MovieInfoTarget.xcconfig"))
+]
+
+let targetSettings: Settings = .settings(configurations: targetConfigurations,
+                                         defaultSettings: .none)
+
+let projectConfigurations: [Configuration] = [
+    .debug(name: .debug, xcconfig: Path("config/MovieInfoProject.xcconfig")),
+    .release(name: .release, xcconfig: Path("config/MovieInfoProject.xcconfig"))
+]
+
+let projectSettings: Settings = .settings(configurations: projectConfigurations,
+                                          defaultSettings: .none)
+
+// MARK: - TARGETS
+
+let movieInfoTarget = Target(name: "MovieInfo",
+                             platform: .iOS,
+                             product: .app,
+                             bundleId: "co.joaoribeiro",
+                             infoPlist: "MovieInfo/Info.plist",
+                             sources: ["MovieInfo/Source/**"],
+                             resources: ["MovieInfo/Resources/**"],
+                             dependencies: [],
+                             settings: targetSettings)
+
+// MARK: - PROJECT
+
+let project = Project(name: "MovieInfo",
+                      organizationName: "joaoribeiroteam",
+                      settings: projectSettings,
+                      targets: [
+                        movieInfoTarget
+                      ])
